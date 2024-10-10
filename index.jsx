c14a7e4dda505e4571cfd4eee96d107d61f0fef9
Radek <radekhendriksz@gmail.com>
2024-10-10 10:52:56 +0200

    course up to 404 not found task

--- index.jsx
+++ index.jsx
@@ -8,23 +8,16 @@ import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
+import NotFound from './pages/Vans/NotFound';
 
 import "./server"
-
-/**
- * Challenge: Add the footer to the site! Make a separate Footer component
- * in the components folder, and just use this as the markup:
- * 
- * <footer>&#169; 2022 #VANLIFE</footer>
- * 
- * I'm mostly wanting to test your ability to find where the footer would
- * be added, and don't care too much about getting the styling correct.
- * But if you feel you need extra CSS practice, feel free to do the styling
- * as well!
- * 
- */
 
 function App() {
   return (
@@ -40,7 +33,14 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
+          <Route path='*' element={<NotFound />}/>
         </Route>
       </Routes>
     </BrowserRouter>
