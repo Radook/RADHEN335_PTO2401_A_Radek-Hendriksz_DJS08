07c40da61d4a23ea1a4fed71169cf17b05acf3f3
Radek <radekhendriksz@gmail.com>
2024-10-11 06:48:59 +0200

    firebase additions added

---
+++ api.js
@@ -0,0 +1,63 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5SJjfl-As3Bjwlh6ELe3Hpkm9e7t_9hk",
+  authDomain: "vanlife-55613.firebaseapp.com",
+  projectId: "vanlife-55613",
+  storageBucket: "vanlife-55613.appspot.com",
+  messagingSenderId: "204346550486",
+  appId: "1:204346550486:web:4523971b1a8365f3878177"
+};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(() => resolve(), ms))
+}
+
+export async function getVans(id) {
+    const url = id ? `/api/vans/${id}` : "/api/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+}
+
+export async function getHostVans(id) {
+    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+}
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
